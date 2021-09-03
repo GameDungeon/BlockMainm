dd330961c40ac5c871c542be9118d216899fea99
GameDungeon <gamedungeon@protonmail.com>
2021-09-02 19:13:23 -0500

    Base

---
+++ site/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass')(require('node-sass'));
+ 
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const cssnext = require('cssnext');
+const stylelint = require("stylelint");
+const purgecss = require('@fullhuman/postcss-purgecss')
+ 
+gulp.task('css', function () {
+    const processors = [
+        stylelint,
+        autoprefixer,
+        //cssnano,
+        cssnext,
+        purgecss({
+            content: ['**/*.html'],
+            css: ['**/*.scss']
+          })
+    ];
+    return gulp.src('./styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist'));
+});
